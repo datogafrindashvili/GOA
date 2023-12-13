a5c5833094546c4b1e786a3e005526e612e3b613
datogafrindashvili <147237994+datogafrindashvili@users.noreply.github.com>
2023-12-13 12:29:11 +0400

    Add files via upload

---
+++ day 11/classwork/day 11.py
@@ -0,0 +1,150 @@
+# def exclamation(word):
+#     print(word + "!")
+
+# exclamation("spam")
+# exclamation("nikastvis")    #exclamation ნიშნავს ძახილის ნიშანს
+
+
+# def exclamation(word):
+#  if word == "spam":
+#     print(word + "!")     #მარტო "spam" ზე იზავს
+
+# exclamation("spam")
+# exclamation("nikastvis")
+
+
+
+# def exclamation(word):
+#  if word == "spam":
+#     print(word)
+#  elif word == "maristvis":
+#    print(word + "<3")
+#  else:
+#   print(word + "!")    
+
+# exclamation("spam")
+# exclamation("nikastvis")   
+# exclamation("maristvis")  
+
+
+# def print_double(x):
+#     print(2 * x)
+# print_double(3)
+
+# def x(y):
+#     print(y + 2)
+
+# x(5)
+
+
+# def print_sum_twice(x, y):
+#     print(x + y)
+#     print(x + y)
+
+# print_sum_twice(5, 8)
+
+
+
+# def family_members_infos(name,age):
+#     print(name + " aris " + age + " wlis ")
+
+
+# family_members_infos(" mate ", str(15))
+# family_members_infos(" saba ", str(16))
+# family_members_infos(" tamuna ", str(25))
+
+# def my_sum(x, y):
+#     return x + y                   # "return" ფუნქცია აბრუნებს ჰამოთვლილ მნიშნელობას და ინახავს მეხსიერებაში // "return" ის შესრულებისას ის ასრულებს ფუნქციის სიცოცხლეს
+
+
+# print(my_sum(5,  8))
+
+
+# def square(x):
+#     return x**2          # 2 ფიფქი ნიშნავს კვადრატში აყვანას
+
+
+
+
+# def sum(x, y):
+#     return x+y
+
+# res = sum(42, 7)
+# print(res)
+
+
+
+# def foo(x, y):
+#     if x >=y:
+#         return(x)
+#     else:
+#         return y
+    
+
+# x = foo(7, 4)
+# print(x)
+
+
+
+
+# def max(x, y):
+#     if x >=y:
+#         return x
+#     else:
+#         return y
+    
+# if max(6, 4) > 10:
+#     print("yes")
+# else:
+#     print("Nope ")
+
+
+# def add_numbers(x,y):
+#     total = x + y
+#     return total
+# print("thin wom't be printed")
+
+# print(add_numbers(4, 5))
+
+
+# def double(a, b):
+#     return [a*2, b*2]
+# x = double(6, 9)
+# print(x)
+
+
+# def calc(x, y):
+#     return[x+y, x*y]
+
+# res = calc(3, 4)
+# print(res[1])
+
+
+# def sum(x):
+
+#     res = 0
+
+#     for i in range(x):
+
+#         res+=1
+
+#     return res
+    
+# print(sum(4))
+
+
+# x = 365
+# y = 7
+
+# print(x % y)
+
+
+
+
+ 
+"""""
+ამას ქვია "dog string" ს ეს არის კომენტარის ნაირი დიდი ტექსტებისთვის
+
+"""""
+
+ 
