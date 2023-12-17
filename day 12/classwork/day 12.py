989d2647ab0c5c4e1b3c1b1c7a3981efa4ab7716
datogafrindashvili <147237994+datogafrindashvili@users.noreply.github.com>
2023-12-17 20:49:10 +0400

    Add files via upload

---
+++ day 12/classwork/day 12.py
@@ -0,0 +1,16 @@
+# def even_or_odd(number):
+#     pass                         
+
+# #"pass" ნიშნავს გაატარე, სიცარიელის ამოსავსებად არის
+
+
+
+# def even_or_odd(number):
+#     if number % 2 ==0:
+#         return "even"
+#     else:
+#         return "odd"
+
+
+# name = "demetre"
+# print(name[0:3])
---
+++ day 12/homework/homework 12.py
@@ -0,0 +1,12 @@
+def find_smallest_int(arr):
+    return min(arr)
+
+
+
+def summation(num):
+    sum = 0
+    for i in range(num + 1):
+        sum += i
+    return sum
+    
+    
